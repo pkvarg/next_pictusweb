1eca39c4f92c613fbc8ad0bec6ee52804484584e
peter varga <pkvarg@yahoo.se>
2024-07-28 19:50:10 +0200

    offer styling

--- app/components/home/Offer.tsx
+++ app/components/home/Offer.tsx
@@ -13,56 +13,54 @@ const Offer = () => {
   const [showOffer4, setShowOffer4] = useState<boolean>(false)
   return (
     <div className='lg:border border-1 mx-4 lg:mx-[10%] mt-8 mb-16 rounded-xl'>
-      <h1 className='text-[35px] mt-8 text-center text-green-500'>
-        {t('offerTitle')}
-      </h1>
-      <div className='flex flex-col justify-center items-center'>
-        <div className='flex flex-col lg:grid lg:grid-cols-2 justify-center items-center lg:items-between my-16 text-[30px] lg:text-[30px] lg:gap-16'>
-          <div className='flex flex-col items-center'>
+      <h1 className='text-[40px] mt-8 text-center'>{t('offerTitle')}</h1>
+      <div className='flex flex-col justify-center items-center mx-4'>
+        <div className='flex flex-col lg:flex-row justify-center items-center lg:items-between my-16 text-[30px] lg:text-[30px] lg:gap-16 lg:w-full'>
+          <div className='flex flex-col gap-4 lg:w-[50%]'>
             <p
-              className='cursor-pointer hover:text-green-500'
+              className='bg-[#1F2937] cursor-pointer text-center p-8 hover:text-green-500 w-full rounded-2xl'
               onClick={() => setShowOffer1((prev) => !prev)}
             >
               {t('offer1')} +
             </p>
             {showOffer1 && (
-              <p className='text-[#93A7B7] text-[25px] text-justify lg:max-w-[250px] my-4'>
+              <p className='text-[#93A7B7] bg-[#1F2937] p-8  text-[25px] text-justify my-4 w-full rounded-2xl'>
                 {t('offer1desc')}
               </p>
             )}
             <p
-              className='cursor-pointer hover:text-green-500'
+              className='bg-[#1F2937] cursor-pointer p-8 text-center hover:text-green-500 rounded-2xl'
               onClick={() => setShowOffer3((prev) => !prev)}
             >
               {t('offer3')} +
             </p>
             {showOffer3 && (
-              <p className='text-[#93A7B7] text-[25px] text-justify lg:max-w-[250px] my-4'>
+              <p className='bg-[#1F2937] text-[#93A7B7] p-8 text-[25px] text-justify w-full my-4 rounded-2xl'>
                 {t('offer3desc')}
               </p>
             )}
           </div>
 
-          <div className='ml-0 lg:ml-[25%] flex flex-col items-center'>
+          <div className='flex flex-col gap-4 lg:w-[50%]'>
             <p
-              className='cursor-pointer hover:text-green-500'
+              className='bg-[#1F2937] cursor-pointer p-8 text-center hover:text-green-500 rounded-2xl'
               onClick={() => setShowOffer2((prev) => !prev)}
             >
               {t('offer2')} +
             </p>
             {showOffer2 && (
-              <p className='text-[#93A7B7] text-[25px] text-justify lg:max-w-[350px] my-4'>
+              <p className=' bg-[#1F2937] text-[#93A7B7] p-8 text-[25px] text-justify w-full my-4 rounded-2xl'>
                 {t('offer2desc')}
               </p>
             )}
             <p
-              className='cursor-pointer hover:text-green-500'
+              className='bg-[#1F2937] cursor-pointer text-center p-8 hover:text-green-500 rounded-2xl'
               onClick={() => setShowOffer4((prev) => !prev)}
             >
               {t('offer4')} +
             </p>
             {showOffer4 && (
-              <p className='text-[#93A7B7] text-[25px] text-justify lg:max-w-[350px] my-4'>
+              <p className='bg-[#1F2937] text-[#93A7B7] p-8 text-[25px] text-justify w-full my-4 rounded-2xl'>
                 {t('offer4desc')}
               </p>
             )}
