7f33592d2d5c42694bae270ff6a498f828e490b8
peter varga <pkvarg@yahoo.se>
2024-08-01 21:07:56 +0200

    TTS init

--- app/[locale]/admin/audio/page.tsx
+++ app/[locale]/admin/audio/page.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+// app/Audio.tsx
+'use client'
+
+import React, { useState } from 'react'
+import { createSpeech } from './../../../components/admin/BackAi'
 
 const Audio = () => {
-  return <div>Audio</div>
+  const [inputText, setInputText] = useState('')
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value)
+  }
+
+  const handleButtonClick = async () => {
+    await createSpeech(inputText)
+    alert('Speech created and saved to the server.')
+  }
+
+  return (
+    <div>
+      <h1>Text to Speech</h1>
+      <input
+        className='text-black pl-2'
+        type='text'
+        value={inputText}
+        onChange={handleInputChange}
+        placeholder='Enter text to convert to speech'
+      />
+      <button onClick={handleButtonClick}>Create Speech</button>
+    </div>
+  )
 }
 
 export default Audio
